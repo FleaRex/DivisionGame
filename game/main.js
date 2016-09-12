c5c4ed826ff36c7d5d6b7b6a80ec3c36bb459c29
FleaRex <bmad29@hotmail.co.uk>
2016-09-12 09:45:25 +0100

    Added start and end spaces.

--- game/main.js
+++ game/main.js
@@ -2,11 +2,11 @@
 
 (function(){
   var Container = PIXI.Container;
-  var graphics = new PIXI.Graphics();
+  var Graphics = PIXI.Graphics;
   var Text = PIXI.Text;
   var Texture = PIXI.Texture;
   var Sprite = PIXI.Sprite;
-
+  var tileDimension = 100;
   //Test that Pixi is working
   console.log(PIXI);
 
@@ -26,9 +26,9 @@
 
   //Create a container object called the `stage`
   var stage = new Container();
-  stage.addChild(graphics);
 
-  var tileTexture = Texture.fromImage('TileSprite.png');
+  var tileTexture = Texture.fromImage(['TileSprite.png']);
+  var skaterTexture = Texture.fromImage(['SkaterSprite.png']);
 
   var gameboard = new Gameboard([[24,72,36,30],
                                  [15,25,16,12],
@@ -37,21 +37,28 @@
   // drawGrid(gameboard);
 
   //Tell the `renderer` to `render` the `stage`
+  drawGrid(gameboard, tileDimension);
   animate();
 
   function animate() {
-    drawGrid(gameboard);
     requestAnimationFrame(animate);
+    drawGrid(gameboard, tileDimension);
     renderer.render(stage);
   }
 
-  function drawGrid(board){
-    var gridContainer = new Container()
-    gridContainer.board = board;
-    stage.addChild(gridContainer);
+  function drawGrid(board, dimension){
+    stage.x = 40;
+    stage.y = 40;
+    stage.board = board;
+    var gridGraphics = new Graphics();
+    stage.addChild(gridGraphics);
+    gridGraphics.lineStyle(2, 0x000000, 1);
+    gridGraphics.beginFill(0xFFFFFF, 1);
+    gridGraphics.drawRect(0, 0, dimension, dimension*board.height);
+    gridGraphics.drawRect(dimension*(board.width + 1), 0, dimension, dimension*board.height);
     for(var column = 0; column < board.width; column++){
       for(var row = 0; row < board.height; row++){
-        drawTile(board.grid[column][row], 100, gridContainer);
+        drawTile(board.grid[column][row], dimension, stage);
       }
     }
   }
@@ -78,9 +85,11 @@
 
     [tileSprite, numberText].forEach(function(tileElement){
       tileElement.y = (tile.i + 0.5)*dimension;
-      tileElement.x = (tile.j + 0.5)*dimension;
+      tileElement.x = (tile.j + 1.5)*dimension;
       tileElement.anchor.x = 0.5;
       tileElement.anchor.y = 0.5;
+      tileElement.scale.x = dimension/100.0;
+      tileElement.scale.y = dimension/100.0;
       container.addChild(tileElement);
     })
 
