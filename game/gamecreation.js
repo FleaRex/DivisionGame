90019b5acc53b052f32b8b1ee9230a0b792a7ce6
FleaRex <bmad29@hotmail.co.uk>
2016-09-20 10:09:56 +0100

    Limit the number of non multiples that are actually multiples.

--- game/gamecreation.js
+++ game/gamecreation.js
@@ -5,8 +5,6 @@ class GameMaker{
     this.timesTables = [6,7,8,9,11,12];
   }
 
-  // TODO: Move ahead one after up or down, make nonMultiple less likely
-  // to give a multiple.
   createNewGame(){
     var timesTable = this.getRandomTimesTable();
     var grid = [];
@@ -14,24 +12,42 @@ class GameMaker{
       grid[i] = []
       for(var j = 0; j < this.length; j++){
         grid[i][j] = this.getNonMultiple(timesTable);
+        // grid[i][j] = 0;
       }
     }
+    // TODO: Bias to start more central.
     var currentCoords = {'i': Math.floor(Math.random() * this.breadth), 'j':0};
+    var lastMove = 'forwards';
     grid[currentCoords.i][currentCoords.j] = this.getMultiple(timesTable);
+    // grid[currentCoords.i][currentCoords.j] = 1;
     while(currentCoords.j < this.length){
-      grid[currentCoords.i][currentCoords.j] = this.getMultiple(timesTable);;
+      grid[currentCoords.i][currentCoords.j] = this.getMultiple(timesTable);
+      // grid[currentCoords.i][currentCoords.j] = 1;
       var pathDecider = Math.random();
       if(pathDecider < 0.35){
-        if(currentCoords.i > 0){
+        if((currentCoords.i > 0) && (lastMove != 'down')){
+          lastMove = 'up';
           currentCoords.i -= 1;
-          grid[currentCoords.i][currentCoords.j] = this.getMultiple(timesTable);;
+          grid[currentCoords.i][currentCoords.j] = this.getMultiple(timesTable);
+          // grid[currentCoords.i][currentCoords.j] = 1;
+        }
+        else{
+          lastMove = 'forwards';
         }
       }
-      else if(pathDecider < 0.7){
+      else if((pathDecider < 0.7) && (lastMove != 'up')){
         if(currentCoords.i < this.breadth - 1){
+          lastMove = 'down';
           currentCoords.i += 1;
-          grid[currentCoords.i][currentCoords.j] = this.getMultiple(timesTable);;
+          grid[currentCoords.i][currentCoords.j] = this.getMultiple(timesTable);
+          // grid[currentCoords.i][currentCoords.j] = 1;
         }
+        else{
+          lastMove = 'forwards';
+        }
+      }
+      else{
+        lastMove = 'forwards';
       }
       currentCoords.j += 1;
     }
@@ -47,12 +63,13 @@ class GameMaker{
 
   // Attempt to limit the number of accidental hits.
   getNonMultiple(number){
+    var maxAttempts = 4;
+    console.log(arguments[1]);
     var value = this.getRandomTimesTable() * this.getRandomTimesTable();
-    if(value % number != 0){
+    if((value % number != 0) || (currentAttempt >= maxAttempts)){
       return value;
-    }
-    else{
-      return this.getRandomTimesTable() * this.getRandomTimesTable();
+    }else {
+      return this.getNonMultiple(number, currentAttempt + 1);
     }
   }
 
